f0d433a3b4a8b6f72f50319b444654a9911b03d9
Michael Duren <michaeld@michaelduren.com>
2025-10-25 12:50:08 -0500

    feat:add custom snippet for solid components

--- nvim/lua/plugins/blink.lua
+++ nvim/lua/plugins/blink.lua
@@ -47,6 +47,8 @@ return {
 			require("blink.cmp").setup(opts)
 			-- Load friendly-snippets
 			require("luasnip.loaders.from_vscode").lazy_load()
+			local snippetPath = vim.fn.stdpath("config") .. "/snippets"
+			require("luasnip.loaders.from_vscode").lazy_load({ paths = { snippetPath } })
 		end,
 	},
 }
